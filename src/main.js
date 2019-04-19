bf8a6441726c782b2688ef4fc3dcd837ca7d8315
King <jinpengguan@126.com>
2019-04-19 17:37:26 +0800

    add all

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './less/index.less'
+import './less/global.less'
+
+class Person {
+    static info = {
+        name:"kim",
+        age : 10
+    }
+}
+
+let a = new Person()
+console.log('a',a)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const htmlWebpackHtml = require('html-webpack-plugin')
+
+module.exports  = {
+    entry :path.join(__dirname, './src/main.js'),
+    output : {
+        path: path.join(__dirname, './dist'),
+        filename:'bundle.js'
+    },
+    devServer: {
+        open : true,
+        port : 3000,
+        // contentBase : 'src',
+        hot : true
+    },
+    module:{
+        rules : [
+            { test : /\.less$/, use : [ 'style-loader', 'css-loader', 'less-loader' ] },
+            { test : /\.js$/, use : {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                  plugins: [require('@babel/plugin-proposal-class-properties')]
+                }  
+            }, exclude : /node_modules/ },
+            { test : /\.(jpg|png|gif|bmp|jepg)$/, use : 'url-loader?limit=10000&name=[hash:8]-[name].[ext]' },
+            { test : /\.(ttf|eot|svg|woff|woff2)$/, use : 'url-loader'}
+        ]
+    },
+    plugins: [
+        new htmlWebpackHtml({
+            title:"CRUISE",
+            template: path.join(__dirname, './src/index.template.html'),
+            filename : 'index.html'
+        })
+    ]
+}
